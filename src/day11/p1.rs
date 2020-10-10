ffcb2cfc612bfca94c6c70b82dff61ba915082bc
Michael Neuenfeld <michael.neuenfeld@gmail.com>
2020-10-10 22:28:33 +0200

    started day11

---
+++ src/day11/p1.rs
@@ -0,0 +1,78 @@
+use std::char;
+
+pub fn doit() {
+    let mut input = String::from("cqjxjnds");
+    println!("Res: {}", calc(&input));
+}
+
+fn calc(input: &String) -> String {
+    loop {
+        let mut c: Vec<u8> = input.chars().map(|x| x as u8).collect();
+        for i in (0..8).rev() {
+            let mut x = *c.get_mut(i).unwrap();
+            x += 1;
+            if x > 'z' as u8 {
+                c[i] = 'a' as u8;
+            } else {
+                c[i] = x;
+                break;
+            }
+        }
+        let d = String::from_utf8(c).unwrap();
+        if check(&d) {
+            return d;
+        }
+    }
+}
+
+fn check(input: &String) -> bool {
+    !input.contains('i') &&
+    !input.contains('o') &&
+    !input.contains('l') &&
+    three_letter(input) &&
+    double_letter(input)
+}
+
+fn three_letter(input: &String) -> bool {
+    let mut res = false;
+    for i in 0..input.len()-2 {
+        let c: Vec<char> = input[i..i+3].chars().collect();
+        //println!("XXX {:#?}", c);
+        if c[0] as u8 + 1 == c[1] as u8 &&
+           c[1] as u8 + 1 == c[2] as u8 {
+               res = true;
+               break;
+           }
+    }
+    res
+}
+
+fn double_letter(input: &String) -> bool {
+    let mut res = false;
+    for i in 0..input.len()-1 {
+        
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_calc() {
+        assert_eq!(super::three_letter(&String::from("abdeg")), false);
+        assert_eq!(super::three_letter(&String::from("abdefhi")), true);
+        assert_eq!(super::three_letter(&String::from("hijklmmn")), true);
+        assert_eq!(super::three_letter(&String::from("abbceffg")), false);
+        assert_eq!(super::double_letter(&String::from("xxxx")), false);
+        assert_eq!(super::double_letter(&String::from("abxxdeyyzu")), true);
+        assert_eq!(super::double_letter(&String::from("abbceffg")), true);
+        assert_eq!(super::double_letter(&String::from("abbcegjk")), false);
+        assert_eq!(super::check(&String::from("hijklmmn")), false);
+        assert_eq!(super::check(&String::from("abbceffg")), false);
+        assert_eq!(super::check(&String::from("abbcegjk")), false);
+        assert_eq!(super::check(&String::from("abcdffaa")), true);
+        assert_eq!(super::check(&String::from("ghjaabcc")), true);        
+        assert_eq!(super::calc(&String::from("abcdefgh")), "abcdffaa");
+        assert_eq!(super::calc(&String::from("ghijklmn")), "ghjaabcc");
+    }
+}
---
+++ src/day11/p2.rs
@@ -0,0 +1,47 @@
+use std::time::SystemTime;
+
+pub fn doit() {
+    let start = SystemTime::now();
+    let mut input: Vec<u8> = vec![3,1,1,3,3,2,2,1,1,3];
+    for _i in 0..54 {
+        input = calc(&input);
+    }
+    println!("Result of day11 p2: {}", input.len());
+    println!("{}ms", SystemTime::now().duration_since(start).unwrap().as_millis());
+}
+
+fn calc(input: &Vec<u8>) -> Vec<u8> {
+    let mut cnt = 1;
+    let mut last: u8 = 0;
+    let mut res = Vec::with_capacity((input.len() as f64 * 1.35).round() as usize);
+    //let mut res = Vec::new();
+    for i in input {
+        if last == 0 {
+            last = *i; 
+            continue; 
+        }
+        if *i == last {
+            cnt += 1;
+        } else {
+            res.push(cnt);
+            res.push(last);
+            last = *i;
+            cnt = 1;
+        }
+    }
+    res.push(cnt);
+    res.push(last);
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_calc() {
+        assert_eq!(super::calc(&vec![1]), vec![1,1]);
+        assert_eq!(super::calc(&vec![1,1]), vec![2,1]);
+        assert_eq!(super::calc(&vec![2,1]), vec![1,2,1,1]);
+        assert_eq!(super::calc(&vec![1,2,1,1]), vec![1,1,1,2,2,1]);
+        assert_eq!(super::calc(&vec![1,1,1,2,2,1]), vec![3,1,2,2,1,1]);
+    }
+}
