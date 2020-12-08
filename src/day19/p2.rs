4e570ce90848a5edca096ced0c87613fbb62f6a2
Michael Neuenfeld <michael.neuenfeld@gmail.com>
2020-12-08 21:05:13 +0100

    day19 p2 brute force

--- src/day19/p2.rs
+++ src/day19/p2.rs
@@ -10,15 +10,15 @@ pub fn doit() {
     println!("Result of day19 p2: {}", res);
 }
 
-fn prep_data(input: &Vec<String>) -> (HashMap<&str, &str>, &str) {
-    let mut rules:HashMap<&str, &str> = HashMap::new();
+fn prep_data(input: &Vec<String>) -> (HashMap<&str, Vec<&str>>, &str) {
+    let mut rules:HashMap<&str, Vec<&str>> = HashMap::new();
     let mut start:Option<&str> = None;
     for line in input {
         if line.contains(" => ") {
             let rp:Vec<&str> = line.split(" => ").collect();
-            match rules.get_mut(rp[1]) {
-                Some(x) => println!("duplicate key {}", x),
-                None => { rules.insert(rp[1], rp[0]); }
+            match rules.get_mut(rp[0]) {
+                Some(x) => x.push(rp[1]),
+                None => { rules.insert(rp[0], vec![rp[1]]); }
             };
         } else if line.len() > 0 {
             start = Some(&line[..]);
@@ -27,12 +27,12 @@ fn prep_data(input: &Vec<String>) -> (HashMap<&str, &str>, &str) {
     (rules, start.unwrap())
 }
 
-fn scalc(input: &HashMap<&str, &str>, d:&str) -> usize {
+fn scalc(input: &HashMap<&str, Vec<&str>>, d:&str) -> usize {
     // println!("XXX1 {:#?}", d);
     let mut res:Vec<String> = Vec::new();
-    res.push(String::from(d));
+    res.push(String::from("e"));
     let mut step:usize = 0;
-    while !res.contains(&String::from("e")) {
+    while !res.contains(&String::from(d)) {
         println!("XXX2 {:#?}", res.len());
         let mut r:Vec<String> = Vec::new();
         for e in res {
@@ -47,11 +47,13 @@ fn scalc(input: &HashMap<&str, &str>, d:&str) -> usize {
     step
 }
 
-fn calc(input: &HashMap<&str, &str>, d:&str) -> Vec<String> {
+fn calc(input: &HashMap<&str, Vec<&str>>, d:&str) -> Vec<String> {
     let mut res:Vec<String> = Vec::new();
     for i in 0..d.len() {
         for (ik, iv) in input.iter().filter(|(&k,_)| i+k.len() <= d.len() && &d[i..i+k.len()] == k) {
-            res.push(String::from(format!("{}{}{}", &d[..i], iv, &d[i+ik.len()..])));
+            for v in iv {
+                res.push(String::from(format!("{}{}{}", &d[..i], v, &d[i+ik.len()..])));
+            }
         }
     }
     // println!("XXX3 {:#?}", res);
