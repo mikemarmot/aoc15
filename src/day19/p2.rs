786c9b21e2d73d6e444ec48099cc5178dd917178
Michael Neuenfeld <michael.neuenfeld@gmail.com>
2020-12-10 19:57:09 +0100

    recursivewq wq

--- src/day19/p2.rs
+++ src/day19/p2.rs
@@ -10,15 +10,15 @@ pub fn doit() {
     println!("Result of day19 p2: {}", res);
 }
 
-fn prep_data(input: &Vec<String>) -> (HashMap<&str, Vec<&str>>, &str) {
-    let mut rules:HashMap<&str, Vec<&str>> = HashMap::new();
+fn prep_data(input: &Vec<String>) -> (HashMap<&str, &str>, &str) {
+    let mut rules:HashMap<&str, &str> = HashMap::new();
     let mut start:Option<&str> = None;
     for line in input {
         if line.contains(" => ") {
             let rp:Vec<&str> = line.split(" => ").collect();
-            match rules.get_mut(rp[0]) {
-                Some(x) => x.push(rp[1]),
-                None => { rules.insert(rp[0], vec![rp[1]]); }
+            match rules.get_mut(rp[1]) {
+                Some(x) => println!("whahh {}", x),
+                None => { rules.insert(rp[1], rp[0]); }
             };
         } else if line.len() > 0 {
             start = Some(&line[..]);
@@ -27,29 +27,24 @@ fn prep_data(input: &Vec<String>) -> (HashMap<&str, Vec<&str>>, &str) {
     (rules, start.unwrap())
 }
 
-fn scalc(input: &HashMap<&str, Vec<&str>>, d:&str) -> usize {
+fn scalc(input: &HashMap<&str, &str>, d:&str) -> usize {
     let mut res:Vec<usize> = Vec::new();
-    let mut cnt_f:u64 = 0;
-    let mut cnt_t:u64 = 0;
-    calc(input, d, "e", 1, &mut res, &mut cnt_f, &mut cnt_t);
+    let mut size:usize = d.len();
+    calc(input, "e", d, 1, &mut res, &mut size);
     *res.iter().min().unwrap()
 }
 
-fn calc(input: &HashMap<&str, Vec<&str>>, s:&str, d:&str, c:usize, res:&mut Vec<usize>, cnt_f:&mut u64, cnt_t:&mut u64) {
+fn calc(input: &HashMap<&str, &str>, s:&str, d:&str, c:usize, res:&mut Vec<usize>, size:&mut usize) {
     for i in 0..d.len() {
         for (ik, iv) in input.iter().filter(|(&k,_)| i+k.len() <= d.len() && &d[i..i+k.len()] == k) {
-            for v in iv {
-                if (1..=475).contains(&c) { println!("{}:{}", c, ik)}
-                let t = format!("{}{}{}", &d[..i], v, &d[i+ik.len()..]);
-                if t == s {
-                    res.push(c);
-                    *cnt_t += 1;
-                } else if t.len() >= s.len() {
-                    *cnt_f += 1;
-                    //if *cnt_f % 1000 == 0 { print!("{:10}//{:10}\r", cnt_f, cnt_t); }
-                } else {
-                    calc(input, s, &t, c+1, res, cnt_f, cnt_t);
-                }
+            println!("c:{} i:{} k:{} -> {}", c, i, ik, iv);
+            let t = format!("{}{}{}", &d[..i], iv, &d[i+ik.len()..]);
+            if t.len() < *size { *size = t.len(); print!("{:5}\r", size)}
+            if t == s {
+                res.push(c);
+                //print!("{} ",c);
+            } else {
+                calc(input, s, &t, c+1, res, size);
             }
         }
     }
